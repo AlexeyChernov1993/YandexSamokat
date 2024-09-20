f95ffe3a6feed577d3722d203a448012e2d25d8c
Alexey <ANChernov@gmail.com>
2024-09-20 13:50:01 +0300

    Final version

---
+++ configuration.py
@@ -0,0 +1,3 @@
+BASE_URL = "https://38a5100b-6ad3-4798-9bf3-5412f64e29c3.serverhub.praktikum-services.ru"
+CREATE_ORDER = "/api/v1/orders"
+ORDER_INFO = "/api/v1/orders/track?t="
---
+++ tests.py
@@ -0,0 +1,7 @@
+import request
+import data
+
+def test_order_info():
+    track_numer = request.create_order(data.body).json()["track"]
+    response = request.order_info(track_numer)
+    assert response.status_code == 200
